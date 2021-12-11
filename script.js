39c3bfce54392fbd87930c7a2a4751032d47c770
AV-Felipe <aerogrado@gmail.com>
2021-12-11 12:39:52 -0300

    Create mock db

--- script.js
+++ script.js
@@ -27,6 +27,26 @@ const lateFeeStandards = {
     
 };
 
+// MOCK DB
+
+const invoiceDB = [
+    {
+        customer: 'Felipe',
+        dueDate: '1985-07-08',
+        value: 100,
+        idNumber: 487065600000,
+        issueDate: '1985-06-08',
+        lateFeeRules: {
+            fine: 0.02,
+            interest: 0.001,
+            interestType: {
+                basis: 'daily',
+                calculationMethod: 'simple'
+            },
+        },
+    },
+];
+
 
 // ELEMENTS
 
@@ -35,6 +55,9 @@ const inputFieldDueDate = document.getElementById('dueDate');
 const inputFieldBaseValue = document.getElementById('baseValue');
 
 const buttonAddInvoice = document.getElementById('addInvoice');
+const buttonUpdateValue = document.getElementById('updateValues');
+
+const outputInvoiceList = document.getElementById('invoiceList');
 
 // FUNCTIONS
 
@@ -45,7 +68,7 @@ function generateInvoiceRow (invoice) {
             <td class="invoiceNumber">${invoice.idNumber}</td>
             <td class="invoiceIssueDate">${invoice.issueDate}</td>
             <td class="invoiceCustomer">${invoice.customer}</td>
-            <td class="invoiceMainValue">${invoice.value}</td>
+            <td class="invoiceMainValue">${invoice.value.toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})}</td>
             <td class="invoiceDueDate">${invoice.dueDate}</td>
             <td class="invoiceCurrentValue"></td>
             <td class="invoiceLateFee"></td>
