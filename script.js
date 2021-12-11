93e8ff0d21c1caa9e465f855eed28c5cc621f9bd
AV-Felipe <aerogrado@gmail.com>
2021-12-11 12:10:57 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// CLASSES
+
+class invoice {
+    constructor (customer, dueDate, value) {
+        this.customer = customer;
+        this.dueDate = dueDate;
+        this.value = value;
+        this.idNumber = Date.now();
+        this.issueDate = this.formatDate();
+        this.lateFeeRules = lateFeeStandards.regularCustomer;
+    }
+    formatDate(){
+        const today = new Date();
+
+        return(today.toISOString().split('T')[0]);
+    }
+};
+
+// CONSTANT OBJECTS
+
+const lateFeeStandards = {
+    regularCustomer: {
+        fine: 0.02,
+        interest: 0.001,
+        interestType: {calculationMethod: 'simple', basis: 'daily'}
+    },
+    
+};
+
+
+// ELEMENTS
+
+const inputFieldCustomerName = document.getElementById('customerName');
+const inputFieldDueDate = document.getElementById('dueDate');
+const inputFieldBaseValue = document.getElementById('baseValue');
+
+const buttonAddInvoice = document.getElementById('addInvoice');
+
+// FUNCTIONS
+
+function generateInvoiceRow (invoice) {
+    return(
+        `
+        <tr class="newInvoice">
+            <td class="invoiceNumber">${invoice.idNumber}</td>
+            <td class="invoiceIssueDate">${invoice.issueDate}</td>
+            <td class="invoiceCustomer">${invoice.customer}</td>
+            <td class="invoiceMainValue">${invoice.value}</td>
+            <td class="invoiceDueDate">${invoice.dueDate}</td>
+            <td class="invoiceCurrentValue"></td>
+            <td class="invoiceLateFee"></td>
+        </tr>
+        `
+    );
+};
+
+
+
