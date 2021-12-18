73ebbd159a1aeea188f26c1d583e8cf0bb5820d4
AV-Felipe <aerogrado@gmail.com>
2021-12-18 12:34:22 -0300

    Add aria expanded attribute to modal

--- script.js
+++ script.js
@@ -289,6 +289,9 @@ function displayFilterMenu(menuToShow) {
     let menuArea = document.querySelector(`.dropdownMenu.${menuToShow}`);
     menuArea.style.display = 'flex';
 
+    const dropDownButton = document.querySelector(`.dropdownMenuIcon.${menuToShow}`);
+    dropDownButton.setAttribute('aria-expanded', 'true');
+
     //defines the table where the button is as the modal area
     const modal = document.getElementById(menuArea.parentNode.id);
     //creates an variable to reference the click inside or outside logic (when outside, removes the listenner we will create next)
@@ -300,6 +303,7 @@ function displayFilterMenu(menuToShow) {
             console.log('clicou fora');
             closeModal()
             document.removeEventListener('click', eventReference);
+            dropDownButton.removeAttribute('aria-expanded');
         }
     };
     //adds an event listener to handle the clicks while the modal is showing
